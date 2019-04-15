15ae41efb808c2811f9084869730c802c8ec2c52
Brandon <Brandon@10.227.144.145>
2019-04-15 18:27:41 -0400

    First commit to the Katalon Setup project.

---
+++ Test Suites/katalonSetup.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>katalonSetup</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a9ba8fbe-8aa8-4aa8-86a2-e7e5d1974e3b</testSuiteGuid>
+</TestSuiteEntity>
